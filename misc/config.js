557073b7c91cafae6c14091dddeb6463bfefa756
andhra21231 <andhrarifqi@icloud.com>
2022-03-16 16:39:45 +0700

    Commit From Other Repo

---
+++ misc/config.js
@@ -0,0 +1,52 @@
+import fs from 'fs';
+
+export let config = {};
+export default config;
+
+export const loadConfig = (filename="config.json") => {
+    let loadedConfig;
+
+    try {
+        loadedConfig = fs.readFileSync(filename, 'utf-8');
+    } catch(e) {
+        try {
+            fs.readFileSync(filename + ".example", 'utf-8');
+            console.error(`You forgot to rename ${filename}.example to ${filename}!`);
+        } catch(e1) {
+            console.error(`Could not find ${filename}!`, e);
+        }
+        return;
+    }
+
+    try {
+        loadedConfig = JSON.parse(loadedConfig);
+    } catch (e) {return console.error(`Could not JSON parse ${filename}! Is it corrupt?`, e)}
+
+    if(!loadedConfig.token || loadedConfig.token === "token goes here")
+        return console.error("You forgot to put your bot token in config.json!");
+
+    loadedConfig.fetchSkinPrices = loadedConfig.showSkinPrices;
+    loadedConfig.fetchSkinRarities = loadedConfig.showSkinRarities;
+
+    applyConfig(loadedConfig, "token", "token goes here");
+    applyConfig(loadedConfig, "fetchSkinPrices", true);
+    applyConfig(loadedConfig, "fetchSkinRarities", true);
+    applyConfig(loadedConfig, "linkItemImage", true);
+    applyConfig(loadedConfig, "refreshSkins", "10 0 0 * * *");
+    applyConfig(loadedConfig, "checkGameVersion", "*/15 * * * *");
+    applyConfig(loadedConfig, "cleanupAccounts", "0 * * * *");
+    applyConfig(loadedConfig, "storePasswords", false);
+
+    saveConfig(filename, config);
+
+    return config;
+}
+
+const saveConfig = (filename, config) => {
+    fs.writeFileSync(filename, JSON.stringify(config, null, 2));
+}
+
+const applyConfig = (loadedConfig, name, defaultValue) => {
+    if(loadedConfig[name] === undefined) config[name] = defaultValue;
+    else config[name] = loadedConfig[name];
+}
---
+++ misc/util.js
@@ -0,0 +1,165 @@
+import https from 'https';
+import fs from 'fs';
+
+const tlsCiphers = [
+    "TLS_AES_128_GCM_SHA256",
+    "TLS_AES_256_GCM_SHA384",
+    "TLS_CHACHA20_POLY1305_SHA256",
+    "TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256",
+    "TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256",
+    "TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384",
+    "TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384",
+    "TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305_SHA256",
+    "TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305_SHA256",
+    "TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA",
+    "TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA",
+    "TLS_RSA_WITH_AES_128_GCM_SHA256",
+    "TLS_RSA_WITH_AES_256_GCM_SHA384",
+    "TLS_RSA_WITH_AES_128_CBC_SHA",
+    "TLS_RSA_WITH_AES_256_CBC_SHA"
+]
+// kontol lu node fetch ngentot ajg bajingan
+export const fetch = (url, options={}) => {
+    return new Promise((resolve) => {
+        const req = https.request(url, {
+            method: options.method || 'GET',
+            headers: options.headers || {},
+            ciphers: tlsCiphers.join(':')
+        }, resp => {
+            const res = {
+                statusCode: resp.statusCode,
+                headers: resp.headers
+                };
+            let chunks = [];
+            resp.on('data', (chunk) => chunk.push(chunk));
+            resp.on('end', () => {
+                res.body = Buffer.concat(chunks).toString(options.encoding || "utf8");
+                resolve(res);
+            });
+            });
+        req.write(options.body || "");
+        req.end();
+    });
+}
+
+//file utils - Implementations Of Forked API
+
+export const asyncReadFile = (file) => {
+    return new Promise(((resolve, reject) => {
+        fs.readFile(path, (err, data) => {
+            if(err) reject(err);
+            else resolve(err);
+            }
+        )
+        }
+    ));
+}
+export const asyncReadJSONFile = async (path) => {
+    return JSON.parse((await asyncReadFile(path)).toString());
+}
+
+export const itemType = {
+    SKIN: "e7c63390-eda7-46e0-bb7a-a6abdacd2433",
+    BUDDY: "dd3bf334-87f3-40bd-b043-682a57a8dc3a",
+    SPRAY: "d5f120f8-ff8c-4aac-92ea-f2b5acbe9475",
+    CARD: "3f296c07-64c3-494c-923b-fe692a4fa1bd",
+    TITLE: "de7caa6b-adf7-4588-bbd1-143831e786c6"
+}
+
+export const parseSetCookie = (setCookie) => {
+    const cookies = {};
+    for(const cookie of setCookie) {
+        const sep = cookie.indexOf("=");
+        cookies[cookie.slice(0, sep)] = cookie.slice(sep + 1, cookie.indexOf(';'));
+    }
+    return cookies;
+}
+
+export const stringifyCookies = (cookies) => {
+    const cookieList = [];
+    for (let [key, value] of Object.entries(cookies)) {
+        cookieList.push(key + "=" + value);
+    }
+    return cookieList.join("; ");
+}
+
+export const extractTokensFromUri = (uri) => {
+    // thx hamper for regex
+    const [, accessToken, idToken] = uri.match(/access_token=((?:[a-zA-Z]|\d|\.|-|_)*).*id_token=((?:[a-zA-Z]|\d|\.|-|_)*).*expires_in=(\d*)/);
+    return [accessToken, idToken]
+}
+
+const decodeToken = (token) => {
+    const encodedPayload = token.split('.')[1];
+    return JSON.parse(atob(encodedPayload));
+}
+
+export const tokenExpiry = (token) => {
+    return decodeToken(token).exp * 1000;
+}
+
+export const MAINTENANCE = "MAINTENANCE";
+
+export const isMaintenance = (json) => {
+    return json.httpStatus === 403 && json.errorCode === "SCHEDULED_DOWNTIME";
+}
+
+export const formatBundle = async (rawBundle) => {
+    const bundle = {
+        uuid: rawBundle.DataAssetID,
+        expires: Math.floor(Date.now() / 1000) + rawBundle.DurationRemainingInSeconds,
+        items: []
+    }
+
+    let price = 0;
+    let basePrice = 0;
+    for(const rawItem of rawBundle.Items) {
+        const item = {
+            uuid: rawItem.Item.ItemID,
+            type: rawItem.Item.ItemTypeID,
+            item: await getItem(rawItem.Item.ItemID, rawItem.Item.ItemTypeID),
+            amount: rawItem.Item.Amount,
+            price: rawItem.DiscountedPrice,
+            basePrice: rawItem.BasePrice,
+            discount: rawItem.DiscountPercent
+        }
+
+        price += item.price;
+        basePrice += item.basePrice;
+
+        bundle.items.push(item);
+    }
+
+    bundle.price = price;
+    bundle.basePrice = basePrice;
+
+    return bundle;
+}
+
+// discord utils
+
+import {rarityEmoji} from "../discord/emoji.js";
+import {MessageActionRow, MessageButton, Permissions, Util} from "discord.js";
+import {getItem} from "../valorant/cache.js";
+
+export const defer = async (interaction, ephemeral=false) => {
+    // discord only sets deferred to true once the event
+    // is sent over ws, which doesn't happen immediately
+    await interaction.deferReply({ephemeral});
+    interaction.deferred = true;
+}
+
+export const skinNameAndEmoji = async (skin, channel) => {
+    if(!skin.rarity) return skin.name;
+    const rarityIcon = await rarityEmoji(channel.guild, skin.rarity.name, skin.rarity.icon, externalEmojisAllowed(channel));
+    return rarityIcon ? `${rarityIcon} ${skin.name}` : skin.name;
+}
+
+export const removeAlertButton = (id, uuid) => new MessageButton().setCustomId(`removealert/${uuid}/${id}/${Math.round(Math.random() * 10000)}`).setStyle("DANGER").setLabel("Remove Alert").setEmoji("✖");
+export const removeAlertActionRow = (id, uuid) => new MessageActionRow().addComponents(removeAlertButton(id, uuid));
+
+// apparently the external emojis in an embed only work if @everyone can use external emojis... probably a bug
+export const externalEmojisAllowed = (channel) => channel.permissionsFor(channel.guild.roles.everyone).has(Permissions.FLAGS.USE_EXTERNAL_EMOJIS);
+export const emojiToString = (emoji) => emoji && `<:${emoji.name}:${emoji.id}>`;
+
+export const escapeMarkdown = Util.escapeMarkdown;
