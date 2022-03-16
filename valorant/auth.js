add262a75c70bb8d90510190a844c43d93fc5748
andhra21231 <andhrarifqi@icloud.com>
2022-03-16 17:23:23 +0700

    Prettier

--- valorant/auth.js
+++ valorant/auth.js
@@ -1,16 +1,20 @@
-import config from '../misc/config';
 import fs from 'fs';
+import config from '../misc/config';
 
-import {fetch, parseSetCookie, stringifyCookies, extractTokensFromUri, tokenExpiry} from "../misc/util";
+import {
+  fetch, parseSetCookie, stringifyCookies, extractTokensFromUri, tokenExpiry,
+} from '../misc/util';
 
 let users;
 
 export const loadUserData = () => {
-    if(!users) try {
-        if(!fs.existsSync("data")) fs.mkdirSync('data');
-        users = JSON.parse(fs.readFileSync("data/users.json", "utf-8"));
-        saveUserData();
-    } catch(e) {
-        users = {};
+  if (!users) {
+    try {
+      if (!fs.existsSync('data')) fs.mkdirSync('data');
+      users = JSON.parse(fs.readFileSync('data/users.json', 'utf-8'));
+      saveUserData();
+    } catch (e) {
+      users = {};
     }
-}
+  }
+};
